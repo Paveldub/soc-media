33d09e85ad67a565297b753f0573741894bdfe0c
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-04-25 19:20:53 +0300

    minimal fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, withRouter } from 'react-router-dom';
-import UsersContainer from './components/Users/UsersContainer';
-import HeaderContainer from './components/Header/HeaderContainer';
-import LoginPage from './components/Login/Login';
+
 import { connect, Provider } from 'react-redux';
 import { compose } from 'redux';
 import { initializeApp } from './redux/app-reducer';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
+import { withSuspense } from './hoc/withSuspense';
+
 import Preloader from './components/common/Preloader/Preloader';
 import store from './redux/redux-store';
-import { withSuspense } from './hoc/withSuspense';
+import UsersContainer from './components/Users/UsersContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import LoginPage from './components/Login/Login';
+import Navbar from './components/Navbar/Navbar';
 
 const DialogsContainer = React.lazy(() =>
   import('./components/Dialogs/DialogsContainer')
