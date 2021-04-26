9d2e61de1e5add88cae9da50df69e45ce99867ea
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-04-26 14:41:53 +0300

    webstorm tests

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from './Header';
-import { connect } from 'react-redux';
-import { logout } from '../../redux/auth-reducer';
+import {connect} from 'react-redux';
+import {logout} from '../../redux/auth-reducer';
 
-class HeaderContainer extends React.Component {
-  render() {
-    return <Header {...this.props} />;
-  }
+const HeaderContainer = (props) => {
+  return <Header {...props} />;
 }
+
 const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
   login: state.auth.login,
